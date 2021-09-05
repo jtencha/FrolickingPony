6e3f74ae7284d1cebc8f656f36ac39e42c909f33
PrancingPony <82825420+FamiliarNameMissing@users.noreply.github.com>
2021-09-05 02:15:49 -0500

    Remove the help command in here

--- cogs/functions.py
+++ cogs/functions.py
@@ -31,30 +31,6 @@ class Functions(commands.Cog):
             embed = discord.Embed(title = "Invite RoboticPony", url = "https://discord.com/oauth2/authorize?bot_id=834799912507277312&permissions=243165293686&scope=bot", description = "Invite the bot with the link above!", color = 0x009933)
             await ctx.send(embed = embed)
 
-        #Help list
-        #Use numbers to navigate the menus
-        #Defaults to ;commands 1 if no number is provided.
-        @bot.command()
-        async def help(ctx, type = "1"):
-            if type == "1":
-                embed = discord.Embed(title = "Avalible Commands:", description = "Commands marked with an * require permissions.\n\n", color = 0x009933)
-                embed.add_field(name = "about [user]", value = "Get info about a user", inline = False)
-                embed.add_field(name = "guetzali", value = "Guetzali moment", inline = False)
-                embed.add_field(name = "help", value = "General overview of commands", inline = False)
-                embed.add_field(name = "invite", value = "Invite RoboticPony", inline = False)
-                embed.add_field(name = "ping", value = "Bot response time.", inline = False)
-                embed.add_field(name = "poll [option] [option] [option] [option]", value = "Create a poll", inline = False)
-                embed.add_field(name = "mute [user] [time] [reason]*", value = "Mute a user for a set time", inline = False)
-                embed.add_field(name = "unmute [user] [reason]*", value = "Unmute a user", inline = False)
-                embed.add_field(name = "kick [user] [reason]*", value = "Kicks a member.", inline = False)
-                embed.add_field(name = "ban [user] [reason]*", value = "Bans a member.", inline = False)
-                embed.add_field(name = "unban [user]*", value = "Unbans a member.", inline = False)
-                embed.add_field(name = "Secret Command*", value = "Puts the bot to sleep.", inline = False)
-                embed.add_field(name = "\n\nList 1 of 1", value = "\n\nAdministrator permissions are recommended for moderation.")
-                await ctx.send(embed = embed)
-            else:
-                await ctx.send("`Invalid command.`")
-
         @bot.command()
         @bot_has_permissions(manage_messages = True)
         async def poll(ctx, option_one, option_two, option_three = None, option_four = None):
